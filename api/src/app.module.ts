0c4845eb5417e3509b1bbc99a27ece2b834b74e1
just-a-simple-human <afanasovkonstantin2005@gmail.com>
2025-04-06 03:41:52 +0300

    /api подключены typeORM и база данных

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get("DATABASE_HOST"),
+      port: configService.get("DATABASE_PORT"),
+      username: configService.get("DATABASE_USERNAME"),
+      password: configService.get("DATABASE_PASSWORD"),
+      database: configService.get("DATABASE_NAME"),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,})
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
