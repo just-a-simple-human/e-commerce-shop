1c34f1b81144e8e608587cea071f0902a74bb58f
just-a-simple-human <afanasovkonstantin2005@gmail.com>
2025-04-17 11:33:34 +0300

    /web initial commit

---
+++ admin-pannel/src/app/ui/app.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes } from "react-router";
+import "./app.css";
+
+export const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </>
+  );
+};
